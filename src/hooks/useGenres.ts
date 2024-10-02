c1ecc9fb0a6071414f68c6dbb209fe8f24c3f2d9
JoeMickey <lingbizhuhou@163.com>
2024-10-02 16:49:03 -0700

    Refactoring-create a reusable API client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,5 @@
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
-import apiClient from '../services/api-client';
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
 import { FetchResponse } from '../services/api-client';
 import genres from '../data/genres'
 
@@ -11,14 +11,13 @@ export interface Genre{
 
 // const useGenres = () => ({data: genres, isLoading: false, error:null});
 
+const apiClient = new APIClient<Genre>('/genres');
+
 
 const useGenres = () => 
     useQuery({
     queryKey:['genres'],
-    queryFn: () => 
-        apiClient
-            .get<FetchResponse<Genre>>('/genres')
-            .then(res => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
     initialData: {count: genres.length, results: genres}
 });
